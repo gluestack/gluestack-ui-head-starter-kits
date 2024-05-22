704b55db37db9762aff52f469f034d026edf88b4
sra1kumar-NULL <sravansandeep163@gmail.com>
2024-05-22 13:06:16 +0530

    fix: upgraded expo projects to expo@51

--- expo-with-gluestack-style/babel.config.js
+++ expo-with-gluestack-style/babel.config.js
@@ -2,13 +2,13 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      'babel-preset-expo',
-      ['@babel/preset-env', { targets: { node: 'current' } }],
-      '@babel/preset-typescript',
+      "babel-preset-expo",
+      ["@babel/preset-env", { targets: { node: "current" } }],
+      "@babel/preset-typescript",
     ],
     plugins: [
       // Required for expo-router
-      'expo-router/babel',
+      // require.resolve("expo-router/babel"),
     ],
   };
 };
